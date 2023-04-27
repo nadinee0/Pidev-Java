a68b9b0c8608c9d2093d3589edb2ac3d1c142c02
bellalounaiheb <bellalouna.iheb@esprit.tn>
2023-04-28 00:25:18 +0100

    Merged first and last name in the display

--- src/tn/leaguestorm/gui/BackController.java
+++ src/tn/leaguestorm/gui/BackController.java
@@ -23,6 +23,7 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import javafx.scene.shape.Circle;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontPosture;
 import tn.leaguestorm.entities.User;
@@ -79,14 +80,19 @@ public class BackController {
 
         for (User u : userList) {
             VBox card = new VBox();
+            card.setAlignment(Pos.CENTER);
             card.setPrefSize(250, 250);
             userPane.setHgap(70);
             userPane.setVgap(20);
-            card.setStyle("-fx-background-color: #ffffff; -fx-border-color: #cccccc; -fx-border-width: 2px; -fx-border-radius: 5px; -fx-padding: 10px; -fx-effect: dropshadow(gaussian, rgba(0,0,0,0.2), 10, 0, 0, 3);");
+            card.setStyle("-fx-background-color: #ffffff; -fx-border-color: #cccccc; -fx-border-width: 2px; -fx-border-radius: 5px; -fx-padding: 10px; -fx-effect: dropshadow(gaussian, rgba(0,0,0,0.2), 10, 0, 0, 3); -fx-clip-shape: circle;");
 
             ImageView imageView;
             try {
-                imageView = new ImageView(new Image(new FileInputStream("C:\\leagueStorm\\src\\tn\\leaguestorm\\miscs\\user\\" + u.getProfilePictureName())));
+                Image image = new Image(new FileInputStream("C:\\leagueStorm\\src\\tn\\leaguestorm\\miscs\\user\\" + u.getProfilePictureName()));
+                imageView = new ImageView(image);
+                imageView.setClip(new Circle(75, 75, 75));
+                imageView.setFitWidth(150);
+                imageView.setFitHeight(150);
                 imageView.setFitWidth(150);
                 imageView.setFitHeight(150);
                 imageView.setPreserveRatio(true);
@@ -95,24 +101,19 @@ public class BackController {
                 Logger.getLogger(BackController.class.getName()).log(Level.SEVERE, null, ex);
             }
 
-            Label firstNameLabel = new Label(u.getFirstName());
-            Label lastNameLabel = new Label(u.getLastName());
+            Label firstAndLastNameLabel = new Label(u.getLastName()+ "."+ u.getFirstName());
             Label emailLabel = new Label(u.getEmail());
 
-            firstNameLabel.setFont(Font.font("Verdana", FontPosture.ITALIC, 16));
-            firstNameLabel.setAlignment(Pos.CENTER);
-            firstNameLabel.setStyle("-fx-text-fill: gray;");
+            firstAndLastNameLabel.setFont(Font.font("Verdana", FontPosture.ITALIC, 16));
+            firstAndLastNameLabel.setAlignment(Pos.CENTER);
+            firstAndLastNameLabel.setStyle("-fx-text-fill: gray;");
 
-            lastNameLabel.setFont(Font.font("Verdana", FontPosture.ITALIC, 16));
-            lastNameLabel.setAlignment(Pos.CENTER);
-            lastNameLabel.setStyle("-fx-text-fill: gray;");
 
             emailLabel.setFont(Font.font("Verdana", FontPosture.ITALIC, 16));
             emailLabel.setAlignment(Pos.CENTER);
             emailLabel.setStyle("-fx-text-fill: gray;");
 
-            card.getChildren().add(firstNameLabel);
-            card.getChildren().add(lastNameLabel);
+            card.getChildren().add(firstAndLastNameLabel);
             card.getChildren().add(emailLabel);
 
             Button disableBtn = new Button(u.isBanned() ? "Unban" : "Ban");
