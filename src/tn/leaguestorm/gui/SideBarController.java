c2df57bdb789eaa777426abc785a47301b226d69
Nadine <nadine.elleuch@esprit.tn>
2023-04-28 05:53:30 +0100

    SideBar Added Front template

---
+++ src/tn/leaguestorm/gui/SideBarController.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tn.leaguestorm.gui;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author Nadine
+ */
+public class SideBarController implements Initializable {
+
+    @FXML
+    private Button btnshop;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void shop(ActionEvent event) throws IOException {
+     FXMLLoader loader = new FXMLLoader(getClass().getResource("/tn/leaguestorm/gui/Shop.fxml"));
+            Parent root = loader.load(); // load the new FXML file
+            Scene scene = new Scene(root); // create a new scene with the new FXML file as its content
+            Node sourceNode = (Node) event.getSource(); // get the source node of the current event
+            Scene currentScene = sourceNode.getScene(); // get the current scene from the source node
+            Stage stage = (Stage) currentScene.getWindow(); // get the current stage
+            stage.setScene(scene); // set the new scene as the content of the stage
+    }
+    
+}
